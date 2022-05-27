0243ee8d9ebab0c65a13b07742d03f5c1356a3a2
techno-tanoC <develop.tanoc@gmail.com>
2022-05-27 23:19:05 +0900

    Add ItemListView

--- app/src/main/java/dev/tanoc/stockin/activity/MainActivity.kt
+++ app/src/main/java/dev/tanoc/stockin/activity/MainActivity.kt
@@ -3,17 +3,13 @@ package dev.tanoc.stockin.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.tanoc.stockin.App
-import dev.tanoc.stockin.component.ItemView
+import dev.tanoc.stockin.component.ItemListView
 import dev.tanoc.stockin.model.Item
 import dev.tanoc.stockin.ui.theme.StockinTheme
 import dev.tanoc.stockin.viewmodel.MainViewModel
@@ -65,22 +61,14 @@ class MainActivity : ComponentActivity() {
         }
 
         val listState = rememberLazyListState()
-        LazyColumn(
-            state = listState,
-            modifier = Modifier
-                .fillMaxSize(),
-        ) {
-            items(items.value) { item ->
-                ItemView(
-                    item,
-                    onClick,
-                    onArchiveClick,
-                    onEditClick,
-                    onDeleteClick,
-                )
-                Divider()
-            }
-        }
+        ItemListView(
+            listState = listState,
+            items = items,
+            onClick = onClick,
+            onArchiveClick = onArchiveClick,
+            onEditClick = onEditClick,
+            onDeleteClick = onDeleteClick,
+        )
     }
 
     @Preview(showBackground = true)
