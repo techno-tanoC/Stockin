19f646b5e34da07420338cf7b4afd78b8429a5e9
techno-tanoC <develop.tanoc@gmail.com>
2022-05-28 00:13:02 +0900

    Revert "Add ItemListView"
    This reverts commit 0243ee8d9ebab0c65a13b07742d03f5c1356a3a2.

--- app/src/main/java/dev/tanoc/stockin/activity/MainActivity.kt
+++ app/src/main/java/dev/tanoc/stockin/activity/MainActivity.kt
@@ -5,15 +5,19 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.tanoc.stockin.App
-import dev.tanoc.stockin.component.ItemListView
+import dev.tanoc.stockin.component.ItemView
 import dev.tanoc.stockin.model.Item
 import dev.tanoc.stockin.ui.theme.StockinTheme
 import dev.tanoc.stockin.viewmodel.MainViewModel
@@ -74,14 +78,22 @@ class MainActivity : ComponentActivity() {
         }
 
         val listState = rememberLazyListState()
-        ItemListView(
-            listState = listState,
-            items = items,
-            onClick = onClick,
-            onArchiveClick = onArchiveClick,
-            onEditClick = onEditClick,
-            onDeleteClick = onDeleteClick,
-        )
+        LazyColumn(
+            state = listState,
+            modifier = Modifier
+                .fillMaxSize(),
+        ) {
+            items(items.value) { item ->
+                ItemView(
+                    item,
+                    onClick,
+                    onArchiveClick,
+                    onEditClick,
+                    onDeleteClick,
+                )
+                Divider()
+            }
+        }
     }
 
     private fun shareUrl(url: String) {
