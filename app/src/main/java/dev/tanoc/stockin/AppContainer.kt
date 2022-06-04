7df5d5c6b99aaefad000a5ed169a14d419dfff8f
techno-tanoC <develop.tanoc@gmail.com>
2022-06-04 17:34:27 +0900

    Add ENDPOINT build config

--- app/src/main/java/dev/tanoc/stockin/AppContainer.kt
+++ app/src/main/java/dev/tanoc/stockin/AppContainer.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class AppContainer(private val context: Context) {
-    private val baseUrl = "http://10.0.2.2:3000"
+    private val baseUrl = BuildConfig.ENDPOINT
     private val retrofit = Retrofit
         .Builder()
         .baseUrl(baseUrl)
