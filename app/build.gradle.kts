41bdfcc1ab97674cbfdec9639a08fe330dd687e1
techno-tanoC <develop.tanoc@gmail.com>
2024-02-21 19:20:06 +0900

    Add coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,9 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 
+    implementation("io.coil-kt:coil:2.5.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
