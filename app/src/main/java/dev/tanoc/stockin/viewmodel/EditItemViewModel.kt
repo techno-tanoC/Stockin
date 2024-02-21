f07fcfc9a45672cfd568ec33ebfc8adf659fea0c
techno-tanoC <develop.tanoc@gmail.com>
2024-02-21 23:05:39 +0900

    Add EditItemViewModel and EditItemActivity

---
+++ app/src/main/java/dev/tanoc/stockin/viewmodel/EditItemViewModel.kt
@@ -0,0 +1,141 @@
+package dev.tanoc.stockin.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.tanoc.stockin.data.ItemRepository
+import dev.tanoc.stockin.data.PrefRepository
+import dev.tanoc.stockin.data.QueryRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+interface EditItemViewModel : UnidirectionalViewModel<EditItemViewModel.State, EditItemViewModel.Effect, EditItemViewModel.Event> {
+    data class State(
+        val id: String,
+        val title: String,
+        val url: String,
+        val thumbnail: String,
+        val isLoading: Boolean,
+    )
+
+    sealed class Effect {
+        object Finish : Effect()
+        data class ShowToast(val message: String) : Effect()
+    }
+
+    sealed class Event {
+        class ChangeTitle(val title: String) : Event()
+        class ChangeUrl(val url: String) : Event()
+        class ChangeThumbnail(val thumbnail: String) : Event()
+        object QueryInfo : Event()
+        object Submit : Event()
+    }
+
+    override val state: StateFlow<State>
+    override val effect: Flow<Effect>
+    override fun event(event: Event)
+}
+
+class RealEditItemViewModel @Inject constructor(
+    private val itemRepository: ItemRepository,
+    private val queryRepository: QueryRepository,
+    private val prefRepository: PrefRepository,
+) : ViewModel(), EditItemViewModel {
+    private val _state = MutableStateFlow(
+        EditItemViewModel.State(
+            id = "",
+            title = "",
+            url = "",
+            thumbnail = "",
+            isLoading = false,
+        )
+    )
+    override val state = _state.asStateFlow()
+
+    private val _effect = MutableSharedFlow<EditItemViewModel.Effect>()
+    override val effect = _effect.asSharedFlow()
+
+    fun init(id: String, title: String, url: String, thumbnail: String) {
+        _state.value = _state.value.copy(
+            id = id,
+            title = title,
+            url = url,
+            thumbnail = thumbnail,
+        )
+    }
+
+    override fun event(event: EditItemViewModel.Event) {
+        viewModelScope.launch {
+            when (event) {
+                is EditItemViewModel.Event.ChangeTitle -> {
+                    _state.value = _state.value.copy(title = event.title)
+                }
+                is EditItemViewModel.Event.ChangeUrl -> {
+                    _state.value = _state.value.copy(url = event.url)
+                }
+                is EditItemViewModel.Event.ChangeThumbnail -> {
+                    _state.value = _state.value.copy(thumbnail = event.thumbnail)
+                }
+                is EditItemViewModel.Event.QueryInfo -> {
+                    queryInfo(_state.value.url)
+                }
+                is EditItemViewModel.Event.Submit -> {
+                    val (id, title, url, thumbnail) = _state.value
+                    submit(id, title, url, thumbnail)
+                }
+            }
+        }
+    }
+
+    private suspend fun queryInfo(url: String) {
+        if (_state.value.isLoading) {
+            return
+        }
+
+        try {
+            _state.value = _state.value.copy(isLoading = true)
+            val pref = prefRepository.prefFlow.first()
+            if (pref != null) {
+                val response = queryRepository.info(pref.token, url)
+                _state.value = _state.value.copy(title = response.title, thumbnail = response.thumbnail)
+            } else {
+                _effect.emit(EditItemViewModel.Effect.ShowToast("Empty token"))
+            }
+        } catch (e: Exception) {
+            Log.e("Stockin EditItemVM", e.stackTraceToString())
+            _effect.emit(EditItemViewModel.Effect.ShowToast("Failed to query the info"))
+        } finally {
+            _state.value = _state.value.copy(isLoading = false)
+        }
+    }
+
+    private suspend fun submit(id: String, title: String, url: String, thumbnail: String) {
+        if (_state.value.isLoading) {
+            return
+        }
+
+        try {
+            _state.value = _state.value.copy(isLoading = true)
+            val pref = prefRepository.prefFlow.first()
+            if (pref != null) {
+                itemRepository.update(pref.token, id, title, url, thumbnail)
+                _effect.emit(EditItemViewModel.Effect.ShowToast("Updated the item"))
+            } else {
+                _effect.emit(EditItemViewModel.Effect.ShowToast("Empty token"))
+            }
+            _effect.emit(EditItemViewModel.Effect.Finish)
+        } catch (e: Exception) {
+            Log.e("Stockin EditItemVM", e.stackTraceToString())
+            _effect.emit(EditItemViewModel.Effect.ShowToast("Failed to edit the item"))
+        } finally {
+            _state.value = _state.value.copy(isLoading = false)
+        }
+    }
+}
