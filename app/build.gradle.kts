68eb8eb78f749404cd1e4a93e1d39f35b8c2431f
techno-tanoC <develop.tanoc@gmail.com>
2024-02-21 23:13:44 +0900

    Add a comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 
+    // Coil
     implementation("io.coil-kt:coil:2.5.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
 
