8accdf55fcfa312dfd1d9a49b14ae90060b930ee
techno-tanoC <develop.tanoc@gmail.com>
2024-02-21 19:30:08 +0900

    Add StockinScaffold

---
+++ app/src/main/java/dev/tanoc/stockin/component/StockinScaffold.kt
@@ -0,0 +1,33 @@
+package dev.tanoc.stockin.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StockinScaffold(
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Stockin")
+                },
+            )
+        },
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it),
+            content = content,
+        )
+    }
+}
