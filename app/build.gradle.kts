5b79a7b5e4054c9dd4e3a7a278d09657feedce04
techno-tanoC <develop.tanoc@gmail.com>
2024-02-21 18:56:44 +0900

    Setup retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,11 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
