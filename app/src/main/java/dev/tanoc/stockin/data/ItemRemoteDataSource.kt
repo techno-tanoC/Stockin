b5507a50516979e2ee8ccf2582f02448bb558a71
techno-tanoC <develop.tanoc@gmail.com>
2024-02-21 18:57:46 +0900

    Add ItemRepository

---
+++ app/src/main/java/dev/tanoc/stockin/data/ItemRemoteDataSource.kt
@@ -0,0 +1,60 @@
+package dev.tanoc.stockin.data
+
+import dev.tanoc.stockin.model.Data
+import dev.tanoc.stockin.model.Item
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+import javax.inject.Inject
+
+data class ItemParams(
+    val title: String,
+    val url: String,
+    val thumbnail: String,
+)
+
+interface ItemService {
+    @GET("/items")
+    suspend fun index(@Header("Authorization") token: String, @Query("before") before: String): Response<Data<List<Item>>>
+
+    @POST("/items")
+    suspend fun create(@Header("Authorization") token: String, @Body params: ItemParams): Response<Data<Item>>
+
+    @PUT("/items/{id}")
+    suspend fun update(@Header("Authorization") token: String, @Path("id") id: String, @Body params: ItemParams): Response<Data<Item>>
+
+    @DELETE("/items/{id}")
+    suspend fun delete(@Header("Authorization") token: String, @Path("id") id: String): Response<Unit>
+}
+
+class ItemRemoteDataSource @Inject constructor(
+    private val itemService: ItemService,
+) {
+    suspend fun index(token: String, before: String): List<Item> {
+        val bearer = "Bearer $token"
+        return itemService.index(bearer, before).body()?.data!!
+    }
+
+    suspend fun create(token: String, title: String, url: String, thumbnail: String): Item {
+        val bearer = "Bearer $token"
+        val params = ItemParams(title, url, thumbnail)
+        return itemService.create(bearer, params).body()?.data!!
+    }
+
+    suspend fun update(token: String, id: String, title: String, url: String, thumbnail: String): Item {
+        val bearer = "Bearer $token"
+        val params = ItemParams(title, url, thumbnail)
+        return itemService.update(bearer, id, params).body()?.data!!
+    }
+
+    suspend fun delete(token: String, id: String) {
+        val bearer = "Bearer $token"
+        itemService.delete(bearer, id).body()
+    }
+}
---
+++ app/src/main/java/dev/tanoc/stockin/data/ItemRepository.kt
@@ -0,0 +1,36 @@
+package dev.tanoc.stockin.data
+
+import javax.inject.Inject
+
+class ItemRepository @Inject constructor(
+    private val localItemDataSource: ItemLocalDataSource,
+    private val remoteItemDataSource: ItemRemoteDataSource,
+) {
+    val itemsFlow = localItemDataSource.itemsFlow
+
+    suspend fun reload(token: String) {
+        val items = remoteItemDataSource.index(token, "ffffffff-ffff-ffff-ffff-ffffffffffff")
+        localItemDataSource.replaceAll(items)
+    }
+
+    suspend fun loadMore(token: String) {
+        val lastId = itemsFlow.value.lastOrNull()?.id ?: "ffffffff-ffff-ffff-ffff-ffffffffffff"
+        val items = remoteItemDataSource.index(token, lastId)
+        localItemDataSource.concat(items)
+    }
+
+    suspend fun create(token: String, title: String, url: String, thumbnail: String) {
+        val item = remoteItemDataSource.create(token, title, url, thumbnail)
+        localItemDataSource.prepend(item)
+    }
+
+    suspend fun update(token: String, id: String, title: String, url: String, thumbnail: String) {
+        val item = remoteItemDataSource.update(token, id, title, url, thumbnail)
+        localItemDataSource.patch(item)
+    }
+
+    suspend fun delete(token: String, id: String) {
+        remoteItemDataSource.delete(token, id)
+        localItemDataSource.remove(id)
+    }
+}
