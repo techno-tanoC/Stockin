6f67643caf43505b50f36d21814ebb3d1839fe58
techno-tanoC <develop.tanoc@gmail.com>
2024-02-21 18:56:44 +0900

    Setup retrofit

---
+++ app/src/main/java/dev/tanoc/stockin/di/RetrofitModule.kt
@@ -0,0 +1,32 @@
+package dev.tanoc.stockin.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dev.tanoc.stockin.BuildConfig
+import dev.tanoc.stockin.data.ItemService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.create
+
+@Module
+@InstallIn(ActivityComponent::class)
+object RetrofitModule {
+    private const val baseUrl = BuildConfig.ENDPOINT
+    private val contentType = "application/json".toMediaType()
+    private val json = Json { ignoreUnknownKeys = true }
+
+    private val retrofit = Retrofit
+        .Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(json.asConverterFactory(contentType))
+        .build()
+
+    @Provides
+    fun bindItemService(): ItemService {
+        return retrofit.create()
+    }
+}
