eb1799ed4305a16674aae58246414373b20cbfc4
techno-tanoC <develop.tanoc@gmail.com>
2024-02-24 22:44:05 +0900

    Move Context.dataStore

--- app/src/main/java/dev/tanoc/stockin/App.kt
+++ app/src/main/java/dev/tanoc/stockin/App.kt
@@ -1,7 +1,13 @@
 package dev.tanoc.stockin
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.HiltAndroidApp
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 @HiltAndroidApp
 class App : Application() {
--- app/src/main/java/dev/tanoc/stockin/TokenStore.kt
+++ app/src/main/java/dev/tanoc/stockin/TokenStore.kt
@@ -1,16 +1,12 @@
 package dev.tanoc.stockin
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 object TokenStore {
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
     private val tokenKey = stringPreferencesKey("token_key")
 
     fun Context.tokenFlow(): Flow<String> {
